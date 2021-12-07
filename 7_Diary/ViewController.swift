38de838f74dd83d9dd726d4aeb187960110467be
YSLee-Dev <ysleedev01@gmail.com>
2021-12-07 18:40:18 +0900

    프로젝트 생성

---
+++ 7_Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7_Diary
+//
+//  Created by 이윤수 on 2021/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
